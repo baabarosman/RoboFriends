a2c653a0fc1b6eaca97c82229a2ac8f8cf160ce1
Al-Hasib <alhasibzn@gmail.com>
2019-11-09 08:25:24 +0600

    Updates

---
+++ precache-manifest.343af9c6f542da0fb19abf183478675f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b853226853a33829bbff136e5142d8f7",
+    "url": "/hasibzn/RoboFriends.git/index.html"
+  },
+  {
+    "revision": "f929f689f6027264244e",
+    "url": "/hasibzn/RoboFriends.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f2e71f6c70a07a98a067",
+    "url": "/hasibzn/RoboFriends.git/static/css/main.45efe195.chunk.css"
+  },
+  {
+    "revision": "f929f689f6027264244e",
+    "url": "/hasibzn/RoboFriends.git/static/js/2.3239a8e8.chunk.js"
+  },
+  {
+    "revision": "f2e71f6c70a07a98a067",
+    "url": "/hasibzn/RoboFriends.git/static/js/main.4908235b.chunk.js"
+  },
+  {
+    "revision": "dc93d582c13164e80a62",
+    "url": "/hasibzn/RoboFriends.git/static/js/runtime-main.6a0d212c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/hasibzn/RoboFriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d466a00c09a2de82ab63493c501be8d.js"
+  "/hasibzn/RoboFriends.git/precache-manifest.343af9c6f542da0fb19abf183478675f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hasibzn/RoboFriends.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
