d68814823695cb57a0769fc105923bb9c44d5d42
Al-Hasib <alhasibzn@gmail.com>
2019-11-09 08:16:11 +0600

    Updates

---
+++ precache-manifest.9d466a00c09a2de82ab63493c501be8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836648bfcc2df190ebe2045969441822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f929f689f6027264244e",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f231c0983782dcb31281",
+    "url": "/static/css/main.751eb1fb.chunk.css"
+  },
+  {
+    "revision": "f929f689f6027264244e",
+    "url": "/static/js/2.3239a8e8.chunk.js"
+  },
+  {
+    "revision": "f231c0983782dcb31281",
+    "url": "/static/js/main.ee016db7.chunk.js"
+  },
+  {
+    "revision": "34071e8c58058500bfbb",
+    "url": "/static/js/runtime-main.4117b52b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
