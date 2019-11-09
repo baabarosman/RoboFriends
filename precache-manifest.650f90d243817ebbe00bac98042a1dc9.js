764b98ac5ccc467dfef3f2b95f6ccb65bb41e4cf
Al-Hasib <alhasibzn@gmail.com>
2019-11-09 08:29:02 +0600

    Updates

---
+++ precache-manifest.650f90d243817ebbe00bac98042a1dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd0293c3f034474a28ed85b5b76b593",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "f929f689f6027264244e",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ddd8b0bc131cc79b8983",
+    "url": "/RoboFriends/static/css/main.3a868379.chunk.css"
+  },
+  {
+    "revision": "f929f689f6027264244e",
+    "url": "/RoboFriends/static/js/2.3239a8e8.chunk.js"
+  },
+  {
+    "revision": "ddd8b0bc131cc79b8983",
+    "url": "/RoboFriends/static/js/main.6cd1672a.chunk.js"
+  },
+  {
+    "revision": "b80432d0211ed593f6a8",
+    "url": "/RoboFriends/static/js/runtime-main.e87d99d9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hasibzn/RoboFriends.git/precache-manifest.343af9c6f542da0fb19abf183478675f.js"
+  "/RoboFriends/precache-manifest.650f90d243817ebbe00bac98042a1dc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hasibzn/RoboFriends.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboFriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
